6841ac29415ce4ef9e55d96be51fe9cbae625032
sandrasubru <161939931+sandrasubru@users.noreply.github.com>
2025-04-01 14:10:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -128,6 +128,6 @@ document.addEventListener("DOMContentLoaded", function () {
     // Display price on payment page
     const priceToPay = localStorage.getItem("price");
     if (priceToPay) {
-        document.getElementById('priceToPay').innerText = ` ₹${priceToPay}  `;
+        document.getElementById('priceToPay').innerText = ` ${priceToPay}  `;
     }
 });
